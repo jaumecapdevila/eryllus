daf80f7f2c286657c0b36f2f6a80b56a1bf331ed
jaumecapdevila <contact@jaumecapdevila.net>
2022-09-28 18:17:53 +0200

    Configure Webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  mode: "development",
+  watch: true,
+  devServer: {
+    static: "./dist",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "eryllus.js",
   },
+  plugins: [new HtmlWebpackPlugin()],
 };
